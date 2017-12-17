f7ae7f57b84409eeb5270b8bbcfb80489edbea1d
Mahaeswaran <34623790+Mahaeswaran@users.noreply.github.com>
2017-12-17 22:33:46 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace rssFeedCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.titleComboBox = new System.Windows.Forms.ComboBox();
+            this.linkLabel = new System.Windows.Forms.LinkLabel();
+            this.descriptionTextBox = new System.Windows.Forms.TextBox();
+            this.channelTextBox = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.saveRssbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(450, 12);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(75, 23);
+            this.refreshButton.TabIndex = 0;
+            this.refreshButton.Text = "Refresh";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Visible = false;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // titleComboBox
+            // 
+            this.titleComboBox.FormattingEnabled = true;
+            this.titleComboBox.Location = new System.Drawing.Point(23, 46);
+            this.titleComboBox.Name = "titleComboBox";
+            this.titleComboBox.Size = new System.Drawing.Size(421, 21);
+            this.titleComboBox.TabIndex = 1;
+            this.titleComboBox.SelectedIndexChanged += new System.EventHandler(this.titleComboBox_SelectedIndexChanged);
+            // 
+            // linkLabel
+            // 
+            this.linkLabel.AutoSize = true;
+            this.linkLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel.Location = new System.Drawing.Point(19, 207);
+            this.linkLabel.Name = "linkLabel";
+            this.linkLabel.Size = new System.Drawing.Size(58, 24);
+            this.linkLabel.TabIndex = 2;
+            this.linkLabel.TabStop = true;
+            this.linkLabel.Text = "GoTo";
+            this.linkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel_LinkClicked);
+            // 
+            // descriptionTextBox
+            // 
+            this.descriptionTextBox.Location = new System.Drawing.Point(23, 77);
+            this.descriptionTextBox.Multiline = true;
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.Size = new System.Drawing.Size(421, 128);
+            this.descriptionTextBox.TabIndex = 3;
+            // 
+            // channelTextBox
+            // 
+            this.channelTextBox.Location = new System.Drawing.Point(23, 237);
+            this.channelTextBox.Name = "channelTextBox";
+            this.channelTextBox.Size = new System.Drawing.Size(418, 20);
+            this.channelTextBox.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(23, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(421, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1_SelectionChangeCommitted);
+            // 
+            // saveRssbutton
+            // 
+            this.saveRssbutton.Location = new System.Drawing.Point(450, 234);
+            this.saveRssbutton.Name = "saveRssbutton";
+            this.saveRssbutton.Size = new System.Drawing.Size(75, 23);
+            this.saveRssbutton.TabIndex = 6;
+            this.saveRssbutton.Text = "Save RSS";
+            this.saveRssbutton.UseVisualStyleBackColor = true;
+            this.saveRssbutton.Click += new System.EventHandler(this.saveRssbutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 306);
+            this.Controls.Add(this.saveRssbutton);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.channelTextBox);
+            this.Controls.Add(this.descriptionTextBox);
+            this.Controls.Add(this.linkLabel);
+            this.Controls.Add(this.titleComboBox);
+            this.Controls.Add(this.refreshButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Rss Feed Reader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.ComboBox titleComboBox;
+        private System.Windows.Forms.LinkLabel linkLabel;
+        private System.Windows.Forms.TextBox descriptionTextBox;
+        private System.Windows.Forms.TextBox channelTextBox;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button saveRssbutton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+
+
+namespace rssFeedCSharp
+{
+    public partial class Form1 : Form
+    {
+
+        //SqlConnection con = new SqlConnection("Data Source= C:/Users/Maha/Documents/Visual Studio 2013/Projects/rssFeedCSharp/rssFeedCSharp/D1.accdb;Persist Security Info=True");
+        //''"Data Source=|DataDirectory|\D1.accdb;Persist Security Info=True;Jet OLEDB:Database Password=1234
+
+        DataTable dt = new DataTable();
+        //SqlDataReader dr;
+        //SqlCommand cmd;
+        public Form1()
+        {
+            using (OleDbConnection conn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:/Users/Maha/Documents/Visual Studio 2013/Projects/rssFeedCSharp/rssFeedCSharp/D1.accdb;Persist Security Info=True;Jet OLEDB:Database Password=1234"))
+
+
+            using (OleDbCommand cmd = new OleDbCommand("select * from rssTable", conn))
+            {
+                conn.Open();
+                //SqlDataAdapter da = new SqlDataAdapter ;
+                //DataTable dt = new DataTable();
+
+                //da.Fill(dt);
+
+                DataTable dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+
+                //string user_role = dt.Rows[0]["link"].ToString();
+                OleDbDataReader reader = cmd.ExecuteReader();
+                reader.Read();
+
+                //for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                //{
+
+                //    this.comboBox1.Items.Add(ds.Tables[0].Rows[i][0] + " " + ds.Tables[0].Rows[i][1] + " " + ds.Tables[0].Rows[i][2]);
+
+                //}
+                //if (reader.HasRows)
+                //           {
+                //               MessageBox.Show("EE");
+                //           }
+                //           else
+                //               MessageBox.Show("QQ");
+                //           }
+                //           {
+                //           }
+                InitializeComponent();
+                //con.Open();
+                //cmd = new SqlCommand("select * from rssTable, con");
+                //dr = cmd.ExecuteReader();
+
+
+            }
+        }
+        String[,] rssData = null;
+
+
+        private String[,] getRssData(String channel)
+        {
+            System.Net.WebRequest myRequest = System.Net.WebRequest.Create(channel);
+            System.Net.WebResponse myResponse = myRequest.GetResponse();
+
+            System.IO.Stream rssStream = myResponse.GetResponseStream();
+            System.Xml.XmlDocument rssDoc = new System.Xml.XmlDocument();
+
+            rssDoc.Load(rssStream);
+
+            System.Xml.XmlNodeList rssItems = rssDoc.SelectNodes("rss/channel/item");
+
+            String[,] tempRssData = new String[100, 3];
+            for (int i = 0; i < rssItems.Count; i++)
+            {
+                System.Xml.XmlNode rssNode;
+
+                rssNode = rssItems.Item(i).SelectSingleNode("title");
+                if (rssNode != null)
+                {
+                    tempRssData[i, 0] = rssNode.InnerText;
+                }
+                else
+                {
+                    tempRssData[i, 0] = "";
+                }
+
+                rssNode = rssItems.Item(i).SelectSingleNode("description");
+                if (rssNode != null)
+                {
+                    tempRssData[i, 1] = rssNode.InnerText;
+
+                }
+                else
+                {
+                    tempRssData[i, 1] = "";
+                }
+
+                rssNode = rssItems.Item(i).SelectSingleNode("link");
+                if (rssNode != null)
+                {
+                    tempRssData[i, 2] = rssNode.InnerText;
+
+                }
+                else
+                {
+                    tempRssData[i, 2] = "";
+                }
+
+
+            }
+            return tempRssData;
+
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            string rssLink = comboBox1.SelectedValue.ToString();
+            titleComboBox.Items.Clear();
+            rssData = getRssData(rssLink);
+            //rssData = getRssData(channelTextBox.Text);
+
+            //string myString = ((ComboBoxItem)comboBox1.SelectedItem).Content.ToString();
+            // rssData = getRssData(comboBox1.SelectedValue.ToString);
+            for (int i = 0; i < rssData.GetLength(0); i++)
+            {
+                if (rssData[i, 0] != null)
+                {
+                    titleComboBox.Items.Add(rssData[i, 0]);
+                }
+                titleComboBox.SelectedIndex = 0;
+            }
+        }
+
+        private void titleComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (rssData[titleComboBox.SelectedIndex, 1] != null) ;
+            descriptionTextBox.Text = rssData[titleComboBox.SelectedIndex, 1];
+            if (rssData[titleComboBox.SelectedIndex, 2] != null) ;
+            linkLabel.Text = "GoTo: " + rssData[titleComboBox.SelectedIndex, 0];
+        }
+        SqlDataAdapter da;
+        DataSet ds = new DataSet();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OleDbConnection connect = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:/Users/Maha/Documents/Visual Studio 2013/Projects/rssFeedCSharp/rssFeedCSharp/D1.accdb;Persist Security Info=True;Jet OLEDB:Database Password=1234");
+            connect.Open();
+            OleDbCommand cmd = new OleDbCommand("select * from rssTable", connect);
+            OleDbDataReader odr = null;
+            odr = cmd.ExecuteReader();
+            DataTable table = new DataTable();
+            table.Load(odr);
+            comboBox1.DataSource = table;
+            comboBox1.BindingContext = this.BindingContext;
+            comboBox1.DisplayMember = "Course Number and Name";
+            comboBox1.ValueMember = "link";
+            connect.Close();
+
+        }
+
+        private void linkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (rssData[titleComboBox.SelectedIndex, 2] != null)
+                System.Diagnostics.Process.Start(rssData[titleComboBox.SelectedIndex, 2]);
+        }
+
+        private void saveRssbutton_Click(object sender, EventArgs e)
+        {
+            string InsertSQL = channelTextBox.Text;
+            using (OleDbConnection conn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:/Users/Maha/Documents/Visual Studio 2013/Projects/rssFeedCSharp/rssFeedCSharp/D1.accdb;Persist Security Info=True;Jet OLEDB:Database Password=1234"))
+
+            using (OleDbCommand cmd = new OleDbCommand("Insert Into rssTable (link) Values ('" + InsertSQL + "')", conn))
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+
+            this.Hide();
+            var myForm = new Form1();
+            myForm.Show();
+
+        }
+
+        private void comboBox1_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            string rssLink = comboBox1.SelectedValue.ToString();
+            titleComboBox.Items.Clear();
+            rssData = getRssData(rssLink);
+            //rssData = getRssData(channelTextBox.Text);
+
+            //string myString = ((ComboBoxItem)comboBox1.SelectedItem).Content.ToString();
+            // rssData = getRssData(comboBox1.SelectedValue.ToString);
+            for (int i = 0; i < rssData.GetLength(0); i++)
+            {
+                if (rssData[i, 0] != null)
+                {
+                    titleComboBox.Items.Add(rssData[i, 0]);
+                }
+                titleComboBox.SelectedIndex = 0;
+            }
+        }
+
+
+    }
+}
